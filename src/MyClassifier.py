67cdb62ea8660abc283ca84ea291224d77eb4f6a
meesf <m.froberg@students.uu.nl>
2019-01-22 14:38:06 +0100

    work in progress

--- src/MyClassifier.py
+++ src/MyClassifier.py
@@ -31,9 +31,9 @@ IMAGE_H = 64
 IMAGE_W = 64
 N_CLASS = 2
 
-test_size = 0.20
-learning_rate = 0.0001
-epochs = 10
+test_size = 0.2
+learning_rate = 0.00003
+epochs = 30
 batch_size = 64
 
 
@@ -113,47 +113,12 @@ def model(images, reuse=False):
             pool_size=[2, 2],
             strides=2
         )
-        # conv4 = tf.layers.conv2d(
-        #     pool3,
-        #     filters=64,
-        #     kernel_size=[5, 5],
-        #     padding="SAME",
-        #     activation=tf.nn.relu
-        # )
-        # pool4 = tf.layers.max_pooling2d(
-        #     conv4,
-        #     pool_size=[2, 2],
-        #     strides=2
-        # )
-        # conv5 = tf.layers.conv2d(
-        #     pool4,
-        #     filters=32,
-        #     kernel_size=[5, 5],
-        #     padding="SAME",
-        #     activation=tf.nn.relu
-        # )
-        # pool5 = tf.layers.max_pooling2d(
-        #     conv5,
-        #     pool_size=[2, 2],
-        #     strides=2
-        # )
         reshape5 = tf.reshape(pool3, [-1, 2048])
         h6 = tf.layers.dropout(tf.layers.dense(reshape5, 256, activation=tf.nn.relu), rate=0.8)
         out = tf.layers.dense(h6, 2, activation=None)
     return out
 
 y = model(x)
-
-# with tf.variable_scope('model') as scope:
-#     h0 = maxpool2d(relu(conv2d(x, 32, k_h=5, k_w=5, d_h=1, d_w=1, name="conv_h0")), 2)
-#     h1 = maxpool2d(relu(conv2d(h0, 64, k_h=5, k_w=5, d_h=1, d_w=1, name="conv_h1")), 2)
-#     h2 = maxpool2d(relu(conv2d(h1, 128, k_h=5, k_w=5, d_h=1, d_w=1, name="conv_h2")), 2)
-#     h3 = maxpool2d(relu(conv2d(h2, 64, k_h=5, k_w=5, d_h=1, d_w=1, name="conv_h3")), 2)
-#     h4 = maxpool2d(relu(conv2d(h3, 32, k_h=5, k_w=5, d_h=1, d_w=1, name="conv_h4")), 2)
-#     h5 = dropout(relu(linear(tf.reshape(h4, [batch_size, -1]), 1024, scope='lin_h5')), rate=0.8)
-#     y = linear(h5, 2)
-
-saver = tf.train.Saver()
 
 cost_function = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=y, labels=y_))
 training_step = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.9, beta2=0.999).minimize(cost_function)
@@ -162,6 +127,8 @@ training_step = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.9, b
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 mse = tf.reduce_mean(tf.square(y - y_))
+
+saver = tf.train.Saver()
 
 sess = tf.Session()
 tf.global_variables_initializer().run(session=sess)
@@ -182,4 +149,10 @@ for epoch in range(epochs):
     test_X, test_Y = get_batch(test_x, test_y, len(test_x), 0, IMAGE_H, IMAGE_W, grayscale=grayscale)
     acc = sess.run(accuracy, feed_dict={x: test_X, y_: test_Y})
     print('Test Accuracy:', acc)
+
+print('saving the model...')
+checkpoint_dir = os.path.join(MODEL_DIR)
+if not os.path.exists(checkpoint_dir):
+    os.makedirs(checkpoint_dir)
+saver.save(sess,  os.path.join(checkpoint_dir, 'classifier'))
 
